20be095c199d202bec2edfcc69f7787cf1fa850a
LGuilhermeMoreira <Lguilhermemoreiraleite@gmail.com>
2024-08-23 09:26:42 -0300

    docker-compose updated

---
+++ authentication-service/database/database.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+	"log"
+	"os"
+	"time"
+)
+
+func openDB(dns string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dns)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func ConnectToDB() *sql.DB {
+	dns := os.Getenv("DNS")
+	var count int64
+	for {
+		db, err := openDB(dns)
+		if err != nil {
+			log.Println("Postgres not yet ready...")
+		} else {
+			log.Println("Connected to database")
+			return db
+		}
+		if count > 10 {
+			fmt.Println(err)
+			return nil
+		}
+		count++
+		log.Println("Wait 2 seconds and try again")
+		time.Sleep(2 * time.Second)
+		continue
+	}
+}
